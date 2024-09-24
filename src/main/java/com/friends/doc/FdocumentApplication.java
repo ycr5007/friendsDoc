f1d94dd546c769e045e6f207a7bf600dd00546da
CiroY <ycr5007@gmail.com>
2024-09-24 14:27:50 +0900

    first commit

---
+++ src/main/java/com/friends/doc/FdocumentApplication.java
@@ -0,0 +1,13 @@
+package com.friends.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdocumentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FdocumentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/friends/doc/FdocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friends.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdocumentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
